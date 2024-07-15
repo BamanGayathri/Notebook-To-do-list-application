b99e102a9306e9b0ad25ac1c1ef106a4f118bfc8
BamanGayathri <153893055+BamanGayathri@users.noreply.github.com>
2024-07-15 12:34:22 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Notes from './pages/Notes';
+import Create from './pages/Create';
+import { createTheme, ThemeProvider } from '@mui/material'
+import { red } from '@mui/material/colors';
+import Layout from './components/Layout';
+
+function App() {
+
+  const theme = createTheme({
+    palette: {
+      secondary: red
+    }
+  })
+  return (
+    <ThemeProvider theme={theme}>
+      <Router >
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Notes />}>
+            </Route>
+            <Route path="/create" element={<Create />}>
+            </Route>
+          </Routes>
+        </Layout>
+      </Router >
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
+
+
